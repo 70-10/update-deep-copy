fee2203d2c88ad8e4e63b858b2309b0639ef7dae
70_10 <7010.dev@gmail.com>
2018-10-18 20:21:05 +0900

    [mod] update example

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 const update = require(".");
 
 const object = { a: 1, b: { c: "c", d: [0, 1, 2, 3] } };
-const copy = update(object, { b: { c: "d" } });
+const copy = update(object, { b: { d: undefined, e: "f" } });
 
-console.log(JSON.stringify(copy, null, 2));
+console.log(copy);
